b164e4b2f21c4eca0b15fe10ec0d6a98d0f81999
Yuri <Yuri@DESKTOP-A7GLIKU>
2025-01-20 21:07:53 +0200

    - Panel with Execute button and Cancel one transfered into separated view - did common name for window with actions with book

---
+++ WpfAppBookManager/View/ExecuteCancelPanelView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace BookLibraryManager.TestApp.View;
+/// <summary>
+/// Interaction logic for ExecuteCancelPanelView.xaml
+/// </summary>
+public partial class ExecuteCancelPanelView : UserControl
+{
+    public ExecuteCancelPanelView()
+    {
+        InitializeComponent();
+    }
+}

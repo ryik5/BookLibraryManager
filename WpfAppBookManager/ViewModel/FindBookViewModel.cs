1b66e37aceebf802658811e3396aa019357b4233
Yuri <Yuri@DESKTOP-A7GLIKU>
2025-01-22 13:05:33 +0200

    refactor: Update FindBookViewModel and remove CloseWindow
    - Removed `CloseWindow.cs` and its `CloseParent` method
    - Added `using BookLibraryManager.Common` in `FindBookViewModel.cs`
    - Added documentation comments to `FindBookViewModel` class and members
    - Changed order of `SearchFields` list to `["Author", "Title", "Pages"]`
    - Updated `FindBookViewModel` to include private fields for properties
    - Moved private fields to be declared after their respective properties
    - Initialized `_finderWindow` as a `readonly` field in constructor
    - Added `CloseWindowCommand` property and its command logic
    - Added `DeleteSelectedBookCommand` property and its command logic
    - Added `CanDeleteBook` method to determine if a book can be deleted
    - Updated `CanSearchBooks` method to check if `BookList` of `_library` is not null

--- WpfAppBookManager/ViewModel/FindBookViewModel.cs
+++ WpfAppBookManager/ViewModel/FindBookViewModel.cs
@@ -5,14 +5,23 @@ using GalaSoft.MvvmLight.CommandWpf;
 
 namespace BookLibraryManager.TestApp.ViewModel;
 
+/// <summary>
+/// ViewModel for finding books in the <see cref="ILibrary"/>.
+/// </summary>
+/// <author>YR 2025-01-21</author>
 public class FindBookViewModel : BindableBase
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FindBookViewModel"/> class.
+    /// </summary>
+    /// <param name="libraryManager">The library manager.</param>
+    /// <param name="library">The library.</param>
     public FindBookViewModel(BookLibraryManager libraryManager, ILibrary library)
     {
         _libraryManager = libraryManager;
         _library = library;
         SearchOnFly = false;
-        SearchFields = ["Title", "Author", "Pages"];
+        SearchFields = ["Author", "Title", "Pages"];
         FindBooksCommand = new RelayCommand(FindBooks, CanSearchBooks);
         DeleteSelectedBookCommand = new RelayCommand(DeleteSelectedBook, CanDeleteBook);
         CloseWindowCommand = new RelayCommand<Window>(CloseWindow);
@@ -21,16 +30,25 @@ public class FindBookViewModel : BindableBase
         _finderWindow.ShowDialog();
     }
 
+    /// <summary>
+    /// Command to find books.
+    /// </summary>
     public RelayCommand FindBooksCommand
     {
         get;
     }
+
+    /// <summary>
+    /// Finds books based on the search text. Updates <see cref="BookList"/>
+    /// </summary>
     private void FindBooks()
     {
         BookList = _libraryManager.FindBooksByTitle(_library, SearchText);
     }
 
-    private string _searchText;
+    /// <summary>
+    /// The search text.
+    /// </summary>
     public string SearchText
     {
         get => _searchText;
@@ -40,8 +58,12 @@ public class FindBookViewModel : BindableBase
                 FindBooks();
         }
     }
+    private string _searchText;
 
     private bool _searchOnFly;
+    /// <summary>
+    /// Value indicating whether to perform search on the fly.
+    /// </summary>
     public bool SearchOnFly
     {
         get => _searchOnFly;
@@ -49,40 +71,58 @@ public class FindBookViewModel : BindableBase
     }
 
     private List<string> _searchFields;
+    /// <summary>
+    /// the fields of the book to perform search.
+    /// </summary>
     public List<string> SearchFields
     {
         get => _searchFields;
         set => SetProperty(ref _searchFields, value);
     }
 
-    private string _selectedSearchField;
+    /// <summary>
+    /// Gets or sets the field of the book to perform search.
+    /// </summary>
     public string SelectedSearchField
     {
         get => _selectedSearchField;
         set => SetProperty(ref _selectedSearchField, value);
     }
+    private string _selectedSearchField;
 
-    private string _textLog;
+    /// <summary>
+    /// Text log of operations
+    /// </summary>
     public string TextLog
     {
         get => _textLog;
         set => SetProperty(ref _textLog, value);
     }
+    private string _textLog;
 
-    private List<Book> _bookList;
+    /// <summary>
+    /// Gets or sets the list of books.
+    /// </summary>
     public List<Book> BookList
     {
         get => _bookList;
         set => SetProperty(ref _bookList, value);
     }
+    private List<Book> _bookList;
 
-    private Book _selectedBook;
+    /// <summary>
+    /// Gets or sets the selected book.
+    /// </summary>
     public Book SelectedBook
     {
         get => _selectedBook;
         set => SetProperty(ref _selectedBook, value);
     }
+    private Book _selectedBook;
 
+    /// <summary>
+    /// Command to close the window.
+    /// </summary>
     public RelayCommand<Window> CloseWindowCommand
     {
         get;
@@ -96,16 +136,26 @@ public class FindBookViewModel : BindableBase
         _finderWindow?.Close();
     }
 
+    /// <summary>
+    /// Command to Delete the selected book.
+    /// </summary>
     public RelayCommand DeleteSelectedBookCommand
     {
         get;
     }
+    /// <summary>
+    /// Deletes the selected book.
+    /// </summary>
     private void DeleteSelectedBook()
     {
         TextLog = _libraryManager.RemoveBook(_library, SelectedBook) ? "Book was deleted successfully" : "Nothing to delete";
         BookList = _libraryManager.FindBooksByTitle(_library, SearchText);
     }
 
+    /// <summary>
+    /// Determines whether a book can be deleted.
+    /// </summary>
+    /// <returns>true if a book is selected; otherwise, false.</returns>
     public bool CanDeleteBook()
     {
         return SelectedBook is Book;
@@ -120,8 +170,7 @@ public class FindBookViewModel : BindableBase
         return _library?.BookList != null;
     }
 
-
-    private FindBookWindow _finderWindow;
+    private readonly FindBookWindow _finderWindow;
     private readonly BookLibraryManager _libraryManager;
     private readonly ILibrary _library;
 }

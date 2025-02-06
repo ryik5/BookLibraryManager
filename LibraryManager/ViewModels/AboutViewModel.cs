ada941fe635e85bcdcd549d5c43ff445cfd79899
Yuri <Yuri@DESKTOP-A7GLIKU>
2025-02-06 22:28:30 +0200

    - added text for 'Abput application'

--- LibraryManager/ViewModels/AboutViewModel.cs
+++ LibraryManager/ViewModels/AboutViewModel.cs
@@ -1,18 +1,22 @@
-﻿using LibraryManager.Models;
+﻿using System.Diagnostics;
+using System.Reflection;
+using LibraryManager.Models;
 
 namespace LibraryManager.ViewModels;
 
 /// <author>YR 2025-02-02</author>
 public class AboutViewModel : BindableBase, IViewModelPageable
 {
-    public string Message
-    {
-        get => _message;
-        set => SetProperty(ref _message, value);
-    }
-
 
     #region Properties
+    public string Header => _headMessage;
+
+    public string Footer
+    {
+        get => _footMessage;
+        set => SetProperty(ref _footMessage, value);
+    }
+
     public string Name => "About";
 
     public bool IsChecked
@@ -23,7 +27,8 @@ public class AboutViewModel : BindableBase, IViewModelPageable
     #endregion
 
     #region Fields
-    private string _message = $"Developer: @YR\nDesigner: @Ila Yavorska\nVersion: {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}";
+    private string _footMessage = $"Developer: @YR\nDesigner: @Ila Yavorska\nVersion: {Assembly.GetExecutingAssembly().GetName().Version}";
+    private readonly string _headMessage = $"App. {FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location).CompanyName}, b.{Assembly.GetExecutingAssembly().GetName().Version}";
     private bool _isChecked;
     #endregion
 }
